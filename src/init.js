480d7fea6e661b100cffc741b558c46d12e7f73f
Hitesh Dholaria <hitesh@pirimidtech.com>
2021-07-14 12:18:31 +0530

    Incorporated the same config/JS changes as of BidFX QA Central app in Hello World app

--- src/init.js
+++ src/init.js
@@ -3,30 +3,27 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 function init(){
-//	alert("Hello, world!");
-	createClient('openfin-test-channel').then(() => {
-		console.log('Invoked createClient');
+	createClient('tradesmart-channel-qa').then(() => {
+		console.log('Central app invoked createClient...');
 	});
 }
 
-
 async function createClient(channelName) {
-   // If the channel has been created this request will be sent to the provider.
-   // If not, the promise will not be resolved or rejected until the channel has been created.
    const clientBus = await fin.InterApplicationBus.Channel.connect(channelName);
+   console.log('Connected to', channelName);
+
+   const response1 = await clientBus.dispatch('tradesmart-channel-connect-qa', {});
+   console.log('Channel provider invoked tradesmart-channel-connect-qa action with payload:', response1);
+   const response2 = await clientBus.dispatch('tradesmart-channel-grant-code-listen-qa', {});
+   console.log('Channel provider invoked tradesmart-channel-grant-code-listen-qa action with payload:', response2);
 
    clientBus.onDisconnection(channelInfo => {
-       // handle the channel lifecycle here - we can connect again which will return a promise
-       // that will resolve if/when the channel is re-created.
        createClient(channelInfo.channelName);
    })
 
-   clientBus.register('openfin-action', (payload, identity) => {
-       // register a callback for a topic to which the channel provider can dispatch an action
-       console.log('Action dispatched by provider: ', JSON.stringify(identity));
-       console.log('Payload sent in dispatch: ', JSON.stringify(payload));
-       return {
-           echo: payload
-       };
+   clientBus.register('tradesmart-channel-grant-code-request-qa', (payload, identity) => {
+       console.log('Channel provider dispatched tradesmart-channel-grant-code-request-qa action:', JSON.stringify(identity));
+       console.log('Channel provider dispatched tradesmart-channel-grant-code-request-qa action:', JSON.stringify(payload));
+       return {account:"pirimid10",grantCode:"qwerty"};
    });
 }
